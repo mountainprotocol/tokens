6f65287648de2f44f47c435ffecad0658cd738f4
Mati <mattiascaricato@gmail.com>
2023-02-05 20:03:49 -0300

    Add shares and reward multiplier

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -6,24 +6,43 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+// import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
+
+import "hardhat/console.sol";
 
 // TODO: Permit
-// TODO: Snapshot
+// TODO: Upgrade (Proxy)
+// TODO: Snapshot?
+
+using SafeMath for uint256;
 
 // Author: @mattiascaricato
 contract Token is ERC20, Ownable, AccessControl, Pausable {
-    mapping(address => bool) internal _blacklist;
+    // using Chainlink for Chainlink.Request;
 
-    event AddressBlacklisted(address indexed addr);
-    event AddressUnBlacklisted(address indexed addr);
+    mapping (address => uint256) private _shares;
+    mapping(address => bool) private _blacklist;
+    uint256 private _rewardMultipler = 1e18;
+    uint256 private _totalShares;
+    uint256 private _totalSupply;
 
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
     bytes32 public constant BLACKLIST_ROLE = keccak256("BLACKLIST_ROLE");
-    
+    bytes32 public constant ORACLE_ROLE = keccak256("ORACLE_ROLE");
+
+    event AddressBlacklisted(address indexed addr);
+    event AddressUnBlacklisted(address indexed addr);
+    event RewardMultiplierUpdated(uint256 indexed addr);
+
     constructor(string memory name_, string memory symbol_, uint256 initialSupply_) ERC20(name_, symbol_) {
         _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
         _mint(_msgSender(), initialSupply_ * (10 ** uint256(decimals())));
+
+        // Chainlink contract addresses Goerli
+        // setChainlinkToken(0x326C977E6efc84E512bB9C30f76E30c160eD06FB);
+        // setChainlinkOracle(0xCC79157eb46F5624204f47AB42b3906cAA40eaB7);
     }
 
     function _beforeTokenTransfer(
@@ -37,7 +56,19 @@ contract Token is ERC20, Ownable, AccessControl, Pausable {
     }
 
     function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
-        _mint(to, amount);
+        require(to != address(0), "ERC20: mint to the zero address");
+
+        _beforeTokenTransfer(address(0), to, amount);
+
+        _totalShares += amount;
+
+        unchecked {
+            // Overflow not possible: balance + amount is at most totalSupply + amount, which is checked above.
+            _shares[to] += amount;
+        }
+        emit Transfer(address(0), to, amount);
+
+        _afterTokenTransfer(address(0), to, amount);
     }
 
     function burn(address from, uint256 amount) public onlyRole(BURNER_ROLE) {
@@ -60,14 +91,134 @@ contract Token is ERC20, Ownable, AccessControl, Pausable {
         return _blacklist[_addr];
     }
 
-    function pause() public onlyOwner {
+    function pause() public virtual onlyOwner {
         super._pause();
     }
 
-    function unpause() public onlyOwner {
+    function unpause() public virtual onlyOwner {
         super._unpause();
     }
 
-    // Wipes the balance of a frozen address, burning the tokens and setting the approval to zero
-    // function wipeBlacklistAddress() {}
+    // function setRewardMultipler() public returns (bytes32) {
+    //     bytes32 jobId = "ca98366cc7314957b8c012c72f05aeeb";
+    //     uint256 fee = (1 * LINK_DIVISIBILITY) / 10;
+
+    //     Chainlink.Request memory req = buildChainlinkRequest(
+    //         jobId,
+    //         owner(),
+    //         this.oracleCallback.selector
+    //     );
+
+    //     req.add("get", "https://reward-multiplier.free.beeceptor.com/");
+
+    //     return sendChainlinkRequest(req, fee);
+    // }
+
+    // function oracleCallback(bytes32 _requestId, uint256 _result) public {
+    //     require(
+    //         msg.sender == chainlinkOracleAddress(),
+    //         "Only the oracle is allowed to call this function"
+    //     );
+
+    //     _rewardMultipler = _result;
+    // }
+
+    function rewardMultipler() public view returns (uint256) {
+        return _rewardMultipler;
+    }
+
+    function setRewardMultipler(uint256 rewardMultipler_) public onlyRole(ORACLE_ROLE) {
+        require(rewardMultipler_ > 0, "Invalid RewardMultiplier");
+        require(rewardMultipler_ < 60000000000000000, "Invalid RewardMultiplier"); // 6bps
+
+        _rewardMultipler = _rewardMultipler.add(rewardMultipler_);
+
+        emit RewardMultiplierUpdated(_rewardMultipler);
+    }
+
+    // TODO: Hardhodear rewards multiplier y agregar lógica de units (ex shares), balanceOf, totalUnits, totalSupply, transfer, mint, burn
+    // shares: returns the number of shares for an account  balanceOf:balanceofaspecificwallet
+    // balanceOf(account) = shares[account] * rewardsMultiplier
+    // totalShares: returns total number of shares underlying USDM  totalSupply: returns total supply of USDM
+    // totalSupply() = totalShares * rewardsMultiplier
+
+    function totalSupply() public view virtual override returns (uint256) {
+        return _totalShares * _rewardMultipler;
+    }
+
+    function totalShares() public view virtual returns (uint256) {
+        return _totalShares;
+    }
+
+    function sharesOf(address account) public view virtual returns (uint256) {
+        return _shares[account];
+    }
+
+    function balanceOf(address account) public view virtual override returns (uint256) {
+        return sharesOf(account) * _rewardMultipler;
+    }
+
 }
+
+
+// Daily 3% APR => 0.00008219178082
+
+
+// BUY - 100 USDC - APY 100%
+
+// Day 0
+// Daily Reward => 0.002739726027
+// Reward Multiplier 1
+// Shares 100
+// balanceOf 100
+
+// Day 1
+// Shares 100
+// Reward Multiplier 1.002739726027
+// balanceOf 100.2739726027
+
+// Day 100
+// Shares 100
+// Reward Multiplier 1.2739726027
+// balanceOf 127.39726027
+
+// Day 200
+// Shares 100
+// Reward Multiplier 1.5479452054
+// balanceOf 154.79452054
+
+// Day 365
+// Shares 100
+// Reward Multiplier 1.9999999
+// balanceOf 199.99999
+
+
+// Redim at day 100
+// Shares 100
+// Reward Multiplier 1.2739726027
+// balanceOf 127.39726027
+
+
+// 1 - 100000000000000000
+// 1000 - 1000000000000000000000
+// *
+// 20% APR
+// Daily => 0.000547945205479452 - 547945205479452
+// Daily => 1.000547945205479452 - 1000547945205479452
+// 1000547945205500000
+
+// 5.479452055e35
+
+
+// 1000000000000000000000 * 1000547945205479452
+// 5.4794521e+32
+// 5.479452055×10³⁵
+// (1000000000000000000000 * 1000547945205479452)/10^18
+// (1000000000000000000000 * 1000547945205479452)/1000000000000000000
+// (1000000000000000000000n * 1000547945205479452n)/1000000000000000000n
+// 1000547945205479452000n
+
+
+// 1000 * 1.0005479452054794521
+// Result: 1,000.5479452054794521 - 1000.547945205479452
+// 1000547945205479452000
--- test/Token.ts
+++ test/Token.ts
@@ -5,7 +5,7 @@ import { ethers } from "hardhat";
 describe("Token", () => {
   const name = "Mountain Protocol USD Token";
   const symbol = "USDM";
-  const totalSupply = 1337;
+  const totalSupply = ethers.utils.parseUnits("1337");
 
   // We define a fixture to reuse the same setup in every test.
   // We use loadFixture to run this setup once, snapshot that state,
@@ -44,15 +44,15 @@ describe("Token", () => {
 
       expect(
         await contract.totalSupply()
-      ).to.equal(ethers.utils.parseUnits(totalSupply.toString()));
+      ).to.equal(totalSupply);
     });
 
     it("should assign the total supply of tokens to the owner", async () => {
       const { contract, owner } = await loadFixture(deployTokenFixture);
 
       expect(
-        ethers.utils.parseUnits(totalSupply.toString())
-      ).to.equal(await contract.balanceOf(owner.address));
+        await contract.balanceOf(owner.address)
+      ).to.equal(totalSupply);
     });
 
     it("should grant admin role to owner", async () => {
@@ -100,7 +100,7 @@ describe("Token", () => {
     it("should emit Transfer events", async () => {
       const { contract, owner, acc1 } = await loadFixture(deployTokenFixture);
 
-      expect(await contract.transfer(acc1.address, 1))
+      await expect(contract.transfer(acc1.address, 1))
         .to.emit(contract, "Transfer")
         .withArgs(owner.address, acc1.address, 1);
     });
@@ -145,13 +145,37 @@ describe("Token", () => {
     it("should burn with burner role", async () => {
       const { contract, owner } = await loadFixture(deployTokenFixture);
       const BURNER_ROLE = ethers.utils.keccak256(ethers.utils.toUtf8Bytes('BURNER_ROLE'));
-      
+
       await contract.grantRole(BURNER_ROLE, owner.address);
 
       await expect(
         contract.burn(owner.address, 1)
       ).to.not.be.revertedWith(
         `AccessControl: account ${owner.address.toLowerCase()} is missing role ${BURNER_ROLE}`
+      );
+    });
+
+    it("should not set the reward multiplier without oracle role", async () => {
+      const { contract, owner } = await loadFixture(deployTokenFixture);
+      const ORACLE_ROLE = ethers.utils.keccak256(ethers.utils.toUtf8Bytes('ORACLE_ROLE'));
+
+      await expect(
+        contract.setRewardMultipler(1)
+      ).to.be.revertedWith(
+        `AccessControl: account ${owner.address.toLowerCase()} is missing role ${ORACLE_ROLE}`
+      );
+    });
+
+    it("should set the reward multiplier with oracle role", async () => {
+      const { contract, owner } = await loadFixture(deployTokenFixture);
+      const ORACLE_ROLE = ethers.utils.keccak256(ethers.utils.toUtf8Bytes('ORACLE_ROLE'));
+
+      await contract.grantRole(ORACLE_ROLE, owner.address);
+
+      await expect(
+        contract.setRewardMultipler(1)
+      ).to.not.be.revertedWith(
+        `AccessControl: account ${owner.address.toLowerCase()} is missing role ${ORACLE_ROLE}`
       );
     });
 
@@ -169,7 +193,7 @@ describe("Token", () => {
     it("should blacklist with blacklist role", async () => {
       const { contract, owner } = await loadFixture(deployTokenFixture);
       const BLACKLIST_ROLE = ethers.utils.keccak256(ethers.utils.toUtf8Bytes('BLACKLIST_ROLE'));
-      
+
       await contract.grantRole(BLACKLIST_ROLE, owner.address);
 
       await expect(
@@ -184,7 +208,7 @@ describe("Token", () => {
     it("should add address to the blacklist", async () => {
       const { contract, owner, acc1 } = await loadFixture(deployTokenFixture);
       const BLACKLIST_ROLE = ethers.utils.keccak256(ethers.utils.toUtf8Bytes('BLACKLIST_ROLE'));
-      
+
       await contract.grantRole(BLACKLIST_ROLE, owner.address);
       await contract.blacklist(acc1.address);
 
@@ -236,8 +260,10 @@ describe("Token", () => {
       const { contract, owner } = await loadFixture(deployTokenFixture);
 
       await expect(
-        contract.pause()
-      ).to.emit(contract, "Paused").withArgs(owner.address);
+        await contract.pause()
+      ).to.not.be.revertedWith(
+        "Ownable: caller is not the owner"
+      );
     });
 
     it("should not pause without admin", async () => {
@@ -248,6 +274,96 @@ describe("Token", () => {
       ).to.be.revertedWith(
         "Ownable: caller is not the owner"
       );
+    });
+  });
+
+  // describe("Oracle", () => {
+  //   it("should update the reward multipler", async () => {
+  //     const { contract, owner } = await loadFixture(deployTokenFixture);
+
+  //     const result = await contract.setRewardMultipler();
+
+  //     console.log(result);
+  //   });
+  // });
+
+  describe("Reward Multiplier", () => {
+    it("should initialize with 1", async () => {
+      const { contract } = await loadFixture(deployTokenFixture);
+
+      expect(
+        await contract.rewardMultipler()
+      ).to.equal(ethers.utils.parseUnits("1"));
+    });
+
+    it("should sum the reward multiplier", async () => {
+      const { contract, owner } = await loadFixture(deployTokenFixture);
+      const ORACLE_ROLE = ethers.utils.keccak256(ethers.utils.toUtf8Bytes('ORACLE_ROLE'));
+
+      await contract.grantRole(ORACLE_ROLE, owner.address);
+
+      // Daily 20% APR
+      const interest = ethers.utils.parseUnits("0.000547945205479452"); // 547945205479452
+      const rewardMultipler = await contract.rewardMultipler();
+
+      const expected = rewardMultipler.add(interest);
+
+      await expect(
+        contract.setRewardMultipler(interest)
+      ).to.emit(contract, "RewardMultiplierUpdated").withArgs(expected);
+    });
+
+    it("should not support reward multiplier below 0", async () => {
+      const { contract, owner } = await loadFixture(deployTokenFixture);
+      const ORACLE_ROLE = ethers.utils.keccak256(ethers.utils.toUtf8Bytes('ORACLE_ROLE'));
+
+      await contract.grantRole(ORACLE_ROLE, owner.address);
+
+      const interest = ethers.utils.parseUnits("0");
+
+      await expect(
+        contract.setRewardMultipler(interest)
+      ).to.be.revertedWith("Invalid RewardMultiplier");
+    });
+
+    it("should not support a reward multiplier above 6bps", async () => {
+      const { contract, owner } = await loadFixture(deployTokenFixture);
+      const ORACLE_ROLE = ethers.utils.keccak256(ethers.utils.toUtf8Bytes('ORACLE_ROLE'));
+
+      await contract.grantRole(ORACLE_ROLE, owner.address);
+
+      const interest = ethers.utils.parseUnits("0.06");
+
+      await expect(
+        contract.setRewardMultipler(interest)
+      ).to.be.revertedWith("Invalid RewardMultiplier");
+    });
+  });
+
+  describe("shares", () => {
+    it("should initialize with zero", async () => {
+      const { contract, owner, acc1 } = await loadFixture(deployTokenFixture);
+
+      expect (await contract.balanceOf(owner.address)).to.equal(0);
+      expect (await contract.balanceOf(acc1.address)).to.equal(0);
+      expect (await contract.sharesOf(owner.address)).to.equal(0);
+      expect (await contract.sharesOf(acc1.address)).to.equal(0);
+    });
+
+    it("should not change amount of shares when updating reward multiplier", async () => {
+      const { contract, owner, acc1 } = await loadFixture(deployTokenFixture);
+      const MINTER_ROLE = ethers.utils.keccak256(ethers.utils.toUtf8Bytes('MINTER_ROLE'));
+      const ORACLE_ROLE = ethers.utils.keccak256(ethers.utils.toUtf8Bytes('ORACLE_ROLE'));
+
+      const sharesToMint = ethers.utils.parseUnits("1337");
+
+      await contract.grantRole(MINTER_ROLE, owner.address);
+      await contract.grantRole(ORACLE_ROLE, owner.address);
+      await contract.mint(owner.address, sharesToMint);
+      await contract.setRewardMultipler(ethers.utils.parseUnits("0.01"));
+
+
+      expect (await contract.sharesOf(owner.address)).to.equal(sharesToMint);
     });
   });
 });
