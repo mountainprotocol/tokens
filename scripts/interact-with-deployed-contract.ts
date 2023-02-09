3d1775111f68d2be7fc0fcf5ce4cf806b9408873
Mati <mattiascaricato@gmail.com>
2023-02-09 20:29:57 -0300

    Update script

--- scripts/interact-with-deployed-contract.ts
+++ scripts/interact-with-deployed-contract.ts
@@ -3,6 +3,11 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
+
+// CHANGE THIS IF NEEDED
+const contractName = 'Token';
+const contractAddress = '0xb68f51ae286aeb9a05d38921cc7a2c008f5e3257';
+
 // Create a node provider
 const network = 'goerli';
 const apiKey = process.env.ALCHEMY_GOERLI_API_KEY;
@@ -14,8 +19,6 @@ const pk = process.env.GOERLI_PRIVATE_KEY;
 const signer = new ethers.Wallet(pk, provider);
 
 // Get deployed contract to interact with
-const contractName = 'Lock';
-const contractAddress = '0xB58DCE7771876C71FE3b1ec4F9845C428971aA88';
 const contract = await ethers.getContractAt(contractName, contractAddress, signer);
 
 // // Low-level way for doing the same
@@ -25,5 +28,5 @@ const contract = await ethers.getContractAt(contractName, contractAddress, signe
 // const contract = contractFactory.attach(contractAddress);
 
 
-// Interact
-await contract.unlockTime();
+// Interact with any public method
+await contract.rewardMultiplier();
