dce3a57a42be994de7d80e2f7c9102cd308ac6de
Mati <mattiascaricato@gmail.com>
2023-02-17 21:02:10 -0300

    Add gas reporter

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,20 +3,29 @@ import "@nomicfoundation/hardhat-toolbox";
 import "@nomiclabs/hardhat-solhint";
 import "@nomicfoundation/hardhat-chai-matchers";
 import "@openzeppelin/hardhat-upgrades";
+import "hardhat-gas-reporter";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const { ETHERSCAN_API_KEY, ALCHEMY_GOERLI_API_KEY, GOERLI_PRIVATE_KEY } = process.env;
+const { ETHERSCAN_API_KEY, ALCHEMY_GOERLI_API_KEY, GOERLI_PRIVATE_KEY, REPORT_GAS, COIN_MARKETCAP_API_KEY } = process.env;
 // const { ETHERSCAN_API_KEY, ALCHEMY_GOERLI_API_KEY, GOERLI_PRIVATE_KEY, ALCHEMY_MAINNET_API_KEY, MAINNET_PRIVATE_KEY } = process.env;
 
-if (!ETHERSCAN_API_KEY || !ALCHEMY_GOERLI_API_KEY || !GOERLI_PRIVATE_KEY) {
+if (!ETHERSCAN_API_KEY || !ALCHEMY_GOERLI_API_KEY || !GOERLI_PRIVATE_KEY || !REPORT_GAS || !COIN_MARKETCAP_API_KEY) {
   console.error('Env variables are not configured');
   process.exit(1);
 }
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+  solidity: {
+    version: "0.8.17",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      },
+    },
+  },
   etherscan: {
     apiKey: ETHERSCAN_API_KEY,
   },
@@ -35,6 +44,11 @@ const config: HardhatUserConfig = {
       chainId: 1337 // We set 1337 to make interacting with MetaMask simpler
     }
   },
+  gasReporter: {
+    enabled: REPORT_GAS === 'true',
+    coinmarketcap: COIN_MARKETCAP_API_KEY,
+    gasPrice: 20
+  }
 };
 
 export default config;
