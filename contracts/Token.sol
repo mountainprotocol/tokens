04ab34aa8d3782b84dc36cd18ab1d8354eec217d
Mati <mattiascaricato@gmail.com>
2023-04-10 09:23:04 +0200

    Improve documentation

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -14,7 +14,7 @@ import "@openzeppelin/contracts-upgradeable/utils/cryptography/ECDSAUpgradeable.
 
 // TODO: Lock functions gracetime period: upgrade + addrewardmultiplier
 
-// Author: @mattiascaricato
+// @author: @mattiascaricato
 contract Token is IERC20Upgradeable, OwnableUpgradeable, AccessControlUpgradeable, PausableUpgradeable, UUPSUpgradeable, IERC20PermitUpgradeable, EIP712Upgradeable {
     using SafeMathUpgradeable for uint256;
     using CountersUpgradeable for CountersUpgradeable.Counter;
@@ -44,6 +44,12 @@ contract Token is IERC20Upgradeable, OwnableUpgradeable, AccessControlUpgradeabl
     event AddressUnBlacklisted(address indexed addr);
     event RewardMultiplier(uint256 indexed addr);
 
+    /**
+     * @notice Initializes the Token contract
+     * @param name_ The name of the token
+     * @param symbol_ The symbol of the token
+     * @param initialShares The initial amount of shares for the contract creator
+     */
     function initialize(string memory name_, string memory symbol_, uint256 initialShares) public initializer {
         _name = name_;
         _symbol = symbol_;
@@ -64,81 +70,98 @@ contract Token is IERC20Upgradeable, OwnableUpgradeable, AccessControlUpgradeabl
         _disableInitializers();
     }
 
+    /**
+     * @notice Ensures that only accounts with the UPGRADER_ROLE can upgrade the contract
+     */
     function _authorizeUpgrade(address) internal override onlyRole(UPGRADER_ROLE) {}
 
     /**
-     * @dev Returns the name of the token.
+     * @notice Returns the name of the token
+     * @return A string representing the token's name
      */
     function name() public view returns (string memory) {
         return _name;
     }
 
     /**
-     * @dev Returns the symbol of the token, usually a shorter version of the
-     * name.
+     * @notice Returns the symbol of the token
+     * @return A string representing the token's symbol
      */
     function symbol() public view returns (string memory) {
         return _symbol;
     }
 
     /**
-     * @dev Returns the number of decimals used to get its user representation.
-     * For example, if `decimals` equals `2`, a balance of `505` tokens should
-     * be displayed to a user as `5.05` (`505 / 10 ** 2`).
-     *
-     * Tokens usually opt for a value of 18, imitating the relationship between
-     * Ether and Wei. This is the default value returned by ERC20 tokens.
-     *
-     * NOTE: This information is only used for _display_ purposes: it in
+     * @notice Returns the number of decimals the token uses
+     * @dev This value is only used for _display_ purposes: it in
      * no way affects any of the arithmetic of the contract, including
      * {IERC20-balanceOf} and {IERC20-transfer}.
+     * @return The number of decimals (18)
      */
     function decimals() public pure returns (uint8) {
         return 18;
     }
 
-     /**
-     * @return the shares that corresponds to `amount` tokens.
+    /**
+     * @notice Converts an amount of tokens to shares
+     * @param amount The amount of tokens to convert
+     * @return The equivalent amount of shares
      */
     function amountToShares(uint256 amount) public view returns (uint256) {
         return amount.mul(BASE).div(rewardMultiplier());
     }
 
     /**
-     * @return the amount of supply that corresponds to `shares`.
+     * @notice Converts an amount of shares to tokens
+     * @param shares The amount of shares to convert
+     * @return The equivalent amount of tokens
      */
     function sharesToAmount(uint256 shares) public view returns (uint256) {
         return shares.mul(rewardMultiplier()).div(BASE);
     }
 
+    /**
+     * @notice Returns the total amount of shares
+     * @return The total amount of shares
+     */
     function totalShares() public view returns (uint256) {
         return _totalShares;
     }
 
     /**
-     * @return the amount of tokens in existence.
-     *
-     * @dev Always equals to `getSupplyByShares()` since token amount
-     * is pegged to the total amount of shares controlled by the protocol.
+     * @notice Returns the total supply of tokens
+     * @return The total supply of tokens
      */
     function totalSupply() public view returns (uint256) {
         return sharesToAmount(_totalShares);
     }
 
+    /**
+     * @notice Returns the amount of shares owned by the account
+     * @param account The account to check
+     * @return The amount of shares owned by the account
+     */
     function sharesOf(address account) public view returns (uint256) {
         return _shares[account];
     }
 
     /**
-     * @return the the account balance owned by `account`.
-     *
+     * @notice Returns the balance of the specified address
      * @dev Balances are dynamic and equal the `account`'s share in the amount of the
      * total reserves controlled by the protocol. See `sharesOf`.
+     * @param account The address to query the balance of
+     * @return The balance of the specified address
      */
     function balanceOf(address account) public view returns (uint256) {
         return sharesToAmount(sharesOf(account));
     }
 
+    /**
+     * @notice Mints a specified number of shares to the given address.
+     * @dev This is an internal function.
+     * @param to The address to which shares will be minted.
+     * @param shares The number of shares to mint.
+     */
     function _mint(address to, uint256 shares) private {
         require(to != address(0), "ERC20: mint to the zero address");
 
@@ -155,6 +178,7 @@ contract Token is IERC20Upgradeable, OwnableUpgradeable, AccessControlUpgradeabl
     }
 
     /**
+     * @notice Mints new tokens to the specified address
      * @dev Creates `shares` and assigns them to `to` account,
      * increasing the total amount of shares not the total supply (directly).
      *
@@ -162,14 +186,24 @@ contract Token is IERC20Upgradeable, OwnableUpgradeable, AccessControlUpgradeabl
      *
      * Requirements:
      *
+     * - Only users with MINTER_ROLE can call this function.
      * - `to` cannot be the zero address.
      * - the contract must not be paused.
+     * @param to The address to mint the tokens to
+     * @param amount The amount of tokens to mint
      */
     function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
         uint256 shares = amountToShares(amount);
         _mint(to, shares);
     }
 
+    /**
+     * @notice Transfers a specified number of shares from one address to another.
+     * @dev This is an internal function.
+     * @param from The address from which shares will be transferred.
+     * @param to The address to which shares will be transferred.
+     * @param shares The number of shares to transfer.
+     */
     function _transferShares(address from, address to, uint256 shares) private {
         require(from != address(0), "ERC20: transfer from the zero address");
         require(to != address(0), "ERC20: transfer to the zero address");
@@ -188,6 +222,12 @@ contract Token is IERC20Upgradeable, OwnableUpgradeable, AccessControlUpgradeabl
         _afterTokenTransfer(from, to, shares);
     }
 
+    /**
+     * @notice Burns a specified number of shares from the given address.
+     * @dev This is an internal function.
+     * @param account The address from which shares will be burned.
+     * @param shares The number of shares to burn.
+     */
     function _burn(address account, uint256 shares) private {
         require(account != address(0), "ERC20: burn from the zero address");
 
@@ -205,15 +245,20 @@ contract Token is IERC20Upgradeable, OwnableUpgradeable, AccessControlUpgradeabl
     }
 
     /**
-     * @notice Destroys `shares` shares from `from` account's holdings,
-     * decreasing the total amount of shares not the total supply (directly).
-     * @dev This doesn't decrease the token total supply.
+     * @notice Burns a specified amount of tokens from the given address.
+     * @dev This function can only be called by an account with the BURNER_ROLE.
+     * It converts the token amount to shares and burns the shares.
      *
-     * Requirements:
+     * Emits a {Transfer} event with `to` set to the zero address.
      *
+     * * Requirements:
+     *
+     * - Only users with BURNER_ROLE can call this function.
      * - `from` cannot be the zero address.
      * - `from` must hold at least `shares` shares.
      * - the contract must not be paused.
+     * @param from The address from which tokens will be burned.
+     * @param amount The amount of tokens to burn.
      */
     function burn(address from, uint256 amount) public onlyRole(BURNER_ROLE) {
         uint256 shares = amountToShares(amount);
@@ -221,12 +266,15 @@ contract Token is IERC20Upgradeable, OwnableUpgradeable, AccessControlUpgradeabl
     }
 
     /**
-     * @dev See {IERC20-transfer}.
-     *
+     * @notice Transfers a specified number of tokens from the caller's address to the recipient.
+     * @dev This function converts the token amount to shares and calls the _transferShares function.
      * Requirements:
      *
      * - `to` cannot be the zero address.
      * - the caller must have a balance of at least `amount`.
+     * @param to The address to which tokens will be transferred.
+     * @param amount The number of tokens to transfer.
+     * @return A boolean value indicating whether the operation succeeded.
      */
     function transfer(address to, uint256 amount) public returns (bool) {
         address owner = _msgSender();
@@ -236,18 +284,31 @@ contract Token is IERC20Upgradeable, OwnableUpgradeable, AccessControlUpgradeabl
         return true;
     }
 
+    /**
+     * @notice Blacklists the specified address
+     * @param account The address to blacklist
+     */
     function blacklist(address account) public onlyRole(BLACKLIST_ROLE) {
         require(!_blacklist[account], "Address already blacklisted");
         _blacklist[account] = true;
         emit AddressBlacklisted(account);
     }
 
+    /**
+     * @notice Removes the specified address from the blacklist
+     * @param account The address to remove from the blacklist
+     */
     function unblacklist(address account) public onlyRole(BLACKLIST_ROLE) {
         require(_blacklist[account], "Address is not blacklisted");
         _blacklist[account] = false;
         emit AddressUnBlacklisted(account);
     }
 
+    /**
+     * @notice Checks if the specified address is blacklisted
+     * @param account The address to check
+     * @return A boolean value indicating whether the address is blacklisted
+     */
     function isBlacklisted(address account) public view returns (bool) {
         return _blacklist[account];
     }
@@ -266,18 +327,35 @@ contract Token is IERC20Upgradeable, OwnableUpgradeable, AccessControlUpgradeabl
         emit Transfer(from, to, amount);
     }
 
+    /**
+     * @notice Pauses token transfers and other operations.
+     * @dev Only the contract owner can call this function. Inherits the _pause function from @openzeppelin/PausableUpgradeable contract.
+     */
     function pause() public onlyOwner {
         super._pause();
     }
 
+    /**
+     * @notice Unpauses token transfers and other operations.
+     * @dev Only the contract owner can call this function. Inherits the _unpause function from @openzeppelin/PausableUpgradeable contract.
+     */
     function unpause() public onlyOwner {
         super._unpause();
     }
 
+    /**
+     * @notice Returns the current reward multiplier
+     * @return The current reward multiplier
+     */
     function rewardMultiplier() public view returns (uint256) {
         return _rewardMultiplier;
     }
 
+    /**
+     * @notice Adds a new reward multiplier to the existing reward multiplier.
+     * @dev Only users with ORACLE_ROLE can call this function.
+     * @param rewardMultiplier_ The new reward multiplier to be added.
+     */
     function addRewardMultiplier(uint256 rewardMultiplier_) public onlyRole(ORACLE_ROLE) {
         require(rewardMultiplier_ > 0, "Invalid RewardMultiplier");
         require(rewardMultiplier_ < 500000000000000, "Invalid RewardMultiplier"); // 5bps
@@ -330,7 +408,11 @@ contract Token is IERC20Upgradeable, OwnableUpgradeable, AccessControlUpgradeabl
     }
 
     /**
+     * @notice Returns the remaining amount of tokens that `spender` is allowed to spend on behalf of `owner`.
      * @dev See {IERC20-allowance}.
+     * @param owner The address of the token owner.
+     * @param spender The address of the spender.
+     * @return The remaining allowance of the spender on behalf of the owner.
      */
     function allowance(address owner, address spender) public view returns (uint256) {
         return _allowances[owner][spender];
@@ -408,20 +490,45 @@ contract Token is IERC20Upgradeable, OwnableUpgradeable, AccessControlUpgradeabl
         return true;
     }
 
+    /**
+     * @notice Returns the EIP-712 DOMAIN_SEPARATOR.
+     * @return A bytes32 value representing the EIP-712 DOMAIN_SEPARATOR.
+     */
     function DOMAIN_SEPARATOR() external view returns (bytes32) {
         return _domainSeparatorV4();
     }
 
+    /**
+     * @notice Returns the current nonce for the given owner address.
+     * @param owner The address whose nonce is to be retrieved.
+     * @return The current nonce as a uint256 value.
+     */
     function nonces(address owner) public view returns (uint256) {
         return _nonces[owner].current();
     }
 
+    /**
+     * @notice Increments and returns the current nonce for a given owner address.
+     * @dev This is an internal function.
+     * @param owner The address whose nonce is to be incremented.
+     */
     function _useNonce(address owner) internal returns (uint256 current) {
         CountersUpgradeable.Counter storage nonce = _nonces[owner];
         current = nonce.current();
         nonce.increment();
     }
 
+    /**
+     * @notice Allows an owner to approve a spender with a one-time signature, bypassing the need for a transaction.
+     * @dev Uses the EIP-2612 standard.
+     * @param owner The address of the token owner.
+     * @param spender The address of the spender.
+     * @param value The amount of tokens to be approved.
+     * @param deadline The expiration time of the signature, specified as a Unix timestamp.
+     * @param v The recovery byte of the signature.
+     * @param r The first 32 bytes of the signature.
+     * @param s The second 32 bytes of the signature.
+     */
     function permit(
         address owner,
         address spender,
