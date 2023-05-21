76d14022383f504e07ae103a080048c68ac3312d
Mati <mattiascaricato@gmail.com>
2023-05-20 21:01:55 -0300

    Add reserved space to allow future version to add new variables

--- contracts/USDM.sol
+++ contracts/USDM.sol
@@ -630,4 +630,11 @@ contract USDM is
 
         _approve(owner, spender, value);
     }
+
+    /**
+     * @dev This empty reserved space is put in place to allow future versions to add new
+     * variables without shifting down storage in the inheritance chain.
+     * See https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps
+     */
+    uint256[50] private __gap;
 }
