e7d2e48022df431f4508283a6dee4f2c5193a07d
Mati <mattiascaricato@gmail.com>
2023-05-25 13:23:09 -0300

    Add forge to CI

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,7 +1,7 @@
 import { HardhatUserConfig } from 'hardhat/config';
 import '@nomicfoundation/hardhat-toolbox';
 import '@nomicfoundation/hardhat-chai-matchers';
-import "@nomicfoundation/hardhat-foundry";
+import '@nomicfoundation/hardhat-foundry';
 import '@openzeppelin/hardhat-upgrades';
 import dotenv from 'dotenv';
 
