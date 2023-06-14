662b23c16a3547bcc18fc9cb2f0e883ade0687a3
Mati <mattiascaricato@gmail.com>
2023-06-14 06:01:37 -0300

    Update storage __gap size

--- contracts/USDM.sol
+++ contracts/USDM.sol
@@ -646,5 +646,5 @@ contract USDM is
      * variables without shifting down storage in the inheritance chain.
      * See https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps
      */
-    uint256[34] private __gap;
+    uint256[42] private __gap;
 }
