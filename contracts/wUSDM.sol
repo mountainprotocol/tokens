a98c49f26545f6c0f67193e3486a95b28eb3975b
Mati <mattiascaricato@gmail.com>
2023-10-16 10:29:53 -0300

    Fix incorrect size of storage gap

--- contracts/wUSDM.sol
+++ contracts/wUSDM.sol
@@ -184,5 +184,5 @@ contract wUSDM is
      * variables without shifting down storage in the inheritance chain.
      * See https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps
      */
-    uint256[49] private __gap;
+    uint256[48] private __gap;
 }
