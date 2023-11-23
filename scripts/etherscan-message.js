5c9685119cec952ac19ba88364f85118d879d9f2
Mati <mattiascaricato@gmail.com>
2023-11-23 15:43:26 -0300

    Update message signing script

--- scripts/etherscan-message.js
+++ scripts/etherscan-message.js
@@ -1,10 +1,3 @@
-// Instructions
-// 1. Install @openzeppelin/defender-relay-client/lib/ethers
-// 2. Create relayer API Key pair
-// 3. Replace values
-// 4. Replace message
-// 4. Run script (node etherscan-message.js)
-
 const { DefenderRelayProvider, DefenderRelaySigner } = require('@openzeppelin/defender-relay-client/lib/ethers');
 const dotenv = require('dotenv');
 
@@ -16,7 +9,7 @@ const credentials = { apiKey: RELAYER_API_KEY, apiSecret: RELAYER_API_SECRET };
 
 const provider = new DefenderRelayProvider(credentials);
 const signer = new DefenderRelaySigner(credentials, provider, { speed: 'fast' });
-const message = '[arbiscan.io 21/11/2023 22:02:35] I, hereby verify that I am the owner/creator of the address [0x59D9356E565Ab3A36dD77763Fc0d87fEaf85508C]';
+const message = '<explorer message>';
 
 const main = async () => {
   const signature = await signer.signMessage(message);
